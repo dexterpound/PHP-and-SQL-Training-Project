8e00150c240e54ae9637ce7f149b9c22009366a0
Peter Cockram <dexterpound@gmail.com>
2019-07-30 11:24:53 -0400

    globe_bank\public\staff\subjects\index.php

---
+++ globe_bank/public/staff/subjects/index.php
@@ -0,0 +1,9 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+	<?php include(SHARED_PATH .'/staff_header.php'); ?>
+	
+	<?php $page_title ='Subjects'; ?>
+  
+	<div id="content"></div>
+  
+	<?php include(SHARED_PATH . '/staff_footer.php'); ?>
